1e39f3f0a29072320490f276a0824089ab5ff61b
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-09-10 22:34:29 +0200

    Adding xml parsing

--- XBMCompanion/MainWindow.xaml.cs
+++ XBMCompanion/MainWindow.xaml.cs
@@ -7,9 +7,17 @@ namespace XBMCompanion
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainViewModel _viewModel;
         public MainWindow()
         {
             InitializeComponent();
+            _viewModel = new MainViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void Search_Clicked(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Search();
         }
     }
 }
---
+++ XBMCompanion/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace XBMCompanion
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
